b203deb84b1c7bfa5c5a2ab916b0098a812a775a
小見大貴 <hirokiomi@omidaikinoMacBook-puro.local>
2019-01-26 12:53:01 +0900

    second commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -13,8 +14,19 @@ class ProfileController extends Controller
       return view('admin.profile.edit');
     }
 
-    public function update()
+    public function update(Request $request)
     {
+      // Varidationを行う
+      $this->validate($requset, Profile::$rules);
+
+      // class Profileをインスタンス化させる
+      $profile = new Profile;
+      $form = $request->all();
+
+      // データベースに保存する
+      $profile = fill($form);
+      $profile = save();
+
       return redirect('admin/profile/edit');
     }
 }
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,7 +20,52 @@
       <div class="container">
         <div class="row">
           <div class="col-md-8 mx-auto">
-            <h2>My profile</h2>
+            <h2>プロフィール</h2>
+            <form class="" action="{{ action('Admin\ProfileController@update') }}"
+            method="post" enctype="multipart/form-data">
+
+            @if (count($errors) > 0)
+            <ul>
+              @foreach ($errors->all() as $e)
+              <li>{{ $e }}</li>
+              @endforeach
+            </ul>
+            @endif
+            <div class="from-group row">
+              <label  class="col-md-2" for="name">氏名</label>
+              <div class="col-md-10">
+                <input type="text" name="name" class="form-control"
+                value="{{ old('name') }}">
+              </div>
+            </div>
+            <div class="from-group row">
+              <label class="col-md-2" for="gender">性別</label>
+              <div class="col-md-10">
+                <input type="checkbox" name="gender" class="from-control"
+                value="man">男性
+                <input type="checkbox" name="gender" class="from-control"
+                value="woman">女性
+              </div>
+            </div>
+            <div class="from-group row">
+              <label class="col-md-2" for="hobby">趣味</label>
+              <div class="col-md-10">
+                <textarea name="hobby" class="from-control" cols="50" rows="10">
+                  {{ old('hobby') }}
+                </textarea>
+              </div>
+            </div>
+            <div class="from-group row">
+              <label class="col-md-2" for="introduction">自己紹介</label>
+              <div class="col-md-10">
+                <textarea name="introduction" calss="from-control" cols="50" rows="20">
+                  {{ old('introduction') }}
+                </textarea>
+              </div>
+            </div>
+            {{ csrf_field() }}
+            <input type="submit" class="btn btn-primary" value="更新">
+            </form>
           </div>
         </div>
       </div>
