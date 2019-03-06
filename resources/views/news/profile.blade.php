40e3065294b1f8cb5b28ad8c4731393a44979473
小見大貴 <hirokiomi@omidaikinoMacBook-puro.local>
2019-03-06 10:29:04 +0900

    heroku test

--- resources/views/news/profile.blade.php
+++ resources/views/news/profile.blade.php
@@ -33,7 +33,7 @@
          <hr color="#c0c0c0">
          <div class="row">
              <div class="posts col-md-8 mx-auto mt-3">
-                 @foreach($posts as $post)
+                @foreach($posts as $post)
                      <div class="post">
                          <div class="row">
                              <div class="text col-md-6">
@@ -44,7 +44,7 @@
                                      {{ str_limit($post->name, 30) }}
                                  </div>
                                  <div class="gender mt-3">
-                                     {{ str_limit($post->gender, 10) }}
+                                     {{ str_limit($post->gender, 5) }}
                                  </div>
                                  <div class="hobby mt-3">
                                      {{ str_limit($post->hobby, 300) }}
